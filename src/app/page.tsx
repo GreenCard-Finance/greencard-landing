c21953f8f6b828f70a4b9aac9097d14f65042607
Tolu okunjoyo <toluokunjoyo@gmail.com>
2026-05-17 22:25:46 +0100

    bug:deployment fail check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/sections/hero";
-import SectionFive from "@/components/sections/section-five";
-import SectionFour from "@/components/sections/section-four";
-import SectionSeven from "@/components/sections/section-seven";
-import SectionSix from "@/components/sections/section-six";
-import SectionThree from "@/components/sections/section-three";
 import SectionOne from "@/components/sections/section-one";
 import SectionTwo from "@/components/sections/section-two";
+import SectionThree from "@/components/sections/section-three";
+import SectionFour from "@/components/sections/section-four";
+import SectionFive from "@/components/sections/section-five";
+import SectionSix from "@/components/sections/section-six";
+import SectionSeven from "@/components/sections/section-seven";
 
 function Home() {
   return (
